f4b842e47947828ca1fc6120f1ec7fa2ff9ef6fc
DoTrongKien <dokien13102007@gmail.com>
2024-11-15 11:21:29 +0700

    com

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -16,6 +16,7 @@ public class PhanMemQLQuanCafe {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Xin chao ae!");
+        System.out.println("Toi la Trong Kien");
     }
     
 }
