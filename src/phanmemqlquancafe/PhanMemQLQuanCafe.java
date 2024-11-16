843fdf0e0c2f02a271da105ed39ffdf428d94cce
hungndweq <hungndth04416@fpt.edu.vn>
2024-11-16 16:48:20 +0700

    com5

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -39,7 +39,7 @@ public class PhanMemQLQuanCafe {
         System.out.println("Toi la Anh Duc1");
         System.out.println("Toi la Tuan Minh");
 
-        System.out.println("Moi ngay 1 commit");
+        System.out.println("Moi ngay 1 commit1");
         System.out.println("Nho commit nhe moi nguoi");
         System.out.println("Huong dan moi nguoi/video");
         System.out.println("Kang Da Gyeom la Duy hung nhe moi nguoi");
