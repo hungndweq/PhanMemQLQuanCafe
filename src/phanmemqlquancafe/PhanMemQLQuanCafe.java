3ff943e85b87f0fd0750a434f134645bd2ca25f4
DoTrongKien <dokien13102007@gmail.com>
2024-11-16 17:24:59 +0700

    comm1

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -4,6 +4,7 @@
  */
 package phanmemqlquancafe;
 import java.time.Year;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 /**
@@ -18,6 +19,28 @@ public class PhanMemQLQuanCafe {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Xin chao ae!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.print("Nhập năm sinh của bạn: ");
+        int namSinh = scanner.nextInt();
+        
+
+        int namHienTai = LocalDate.now().getYear();
+        
+
+        int tuoi = namHienTai - namSinh;
+        
+
+        if (tuoi >= 0) {
+            System.out.println("Tuổi của bạn là: " + tuoi + " tuổi.");
+        } else {
+            System.out.println("Năm sinh không hợp lệ!");
+        }
+
+        scanner.close();
+    
     }
     
 }
