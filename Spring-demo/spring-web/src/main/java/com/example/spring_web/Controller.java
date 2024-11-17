11bef3ca98b226c2b200ad3e2e786c317688260a
hungndweq <hungndth04416@fpt.edu.vn>
2024-11-17 12:05:24 +0700

    Spring test

---
+++ Spring-demo/spring-web/src/main/java/com/example/spring_web/Controller.java
@@ -0,0 +1,13 @@
+package com.example.springweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello.html";  // Trả về tên của template hello.html trong thư mục templates
+    }
+}
