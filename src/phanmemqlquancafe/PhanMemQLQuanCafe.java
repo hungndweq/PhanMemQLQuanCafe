52f762e314cf022740fc57a349e7e0a2a24a517f
hungndweq <hungndth04416@fpt.edu.vn>
2024-11-17 11:12:23 +0700

    Update

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -4,6 +4,9 @@
  */
 package phanmemqlquancafe;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author DELL
@@ -33,7 +36,6 @@ public class PhanMemQLQuanCafe {
         System.out.println("Xin chao ae!");
 
         System.out.println("Toi la Trung Kien");
-      
 
         System.out.println("al1");
         System.out.println("Toi la Anh Duc1");
@@ -43,8 +45,86 @@ public class PhanMemQLQuanCafe {
         System.out.println("Nho commit nhe moi nguoi");
         System.out.println("Huong dan moi nguoi/video");
         System.out.println("Kang Da Gyeom la Duy hung nhe moi nguoi");
-        
+        int rows = 10; // Số dòng tam giác Pascal
 
+        for (int i = 0; i < rows; i++) {
+            int number = 1;
+
+            // Thêm khoảng trắng đầu dòng
+            for (int k = 0; k < rows - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+
+        }
+        System.out.println("     J    A   V     V   A");
+        System.out.println("     J   A A   V   V   A A");
+        System.out.println(" J   J  AAAAA   V V   AAAAA");
+        System.out.println("  JJJ  A     A   V   A     A");
+        String text = "JAVA ROCKS!";
+        String[] colors = {
+            "\033[31m", // Red
+            "\033[32m", // Green
+            "\033[33m", // Yellow
+            "\033[34m", // Blue
+            "\033[35m", // Magenta
+            "\033[36m" // Cyan
+        };
+
+        for (int i = 0; i < text.length(); i++) {
+            System.out.print(colors[i % colors.length] + text.charAt(i));
+        }
+        System.out.println("\033[0m"); // Reset color
+        // Màu sắc bằng ANSI Escape Codes
+        String[] colors1 = {
+            "\033[31m", // Đỏ
+            "\033[33m", // Vàng
+            "\033[32m", // Xanh lá
+            "\033[34m", // Xanh dương
+            "\033[35m", // Tím
+            "\033[36m", // Xanh cyan
+            "\033[37m" // Trắng
+        };
+
+        // Chuỗi chữ cần in
+        String text1 = "NETFLIX";
+
+        // In từng ký tự với màu sắc
+        for (int i = 0; i < text1.length(); i++) {
+            System.out.print(colors[i % colors.length] + text1.charAt(i));
+        }
+
+        // Reset màu về bình thường
+        System.out.println("\033[0m");
+        String RED = "\033[31m";
+        String WHITE = "\033[37m";
+        String RESET = "\033[0m";
+
+        // Chuỗi ASCII Art cho chữ "NETFLIX"
+        String[] netflixArt = {
+            "#####  ########  #######  ######  ######  ##     ## ",
+            "##  ## ##       ##     ## ##   ## ##   ## ###   ### ",
+            "##     ##       ##     ## ##   ## ##   ## #### #### ",
+            "#####  ######   ##     ## ######  ######  ## ### ## ",
+            "    ## ##       ##     ## ##      ##      ##     ## ",
+            "##  ## ##       ##     ## ##      ##      ##     ## ",
+            "#####  ########  #######  ##      ##      ##     ## "
+        };
+
+        // In từng dòng với màu sắc
+        for (int i = 0; i < netflixArt.length; i++) {
+            if (i % 2 == 0) { // Dòng chẵn màu đỏ
+                System.out.println(RED + netflixArt[i] + RESET);
+            } else { // Dòng lẻ màu trắng
+                System.out.println(WHITE + netflixArt[i] + RESET);
+            }
+        }
     }
-    
+
 }
