19737577e7a3cc60c8dd22f4af25436e91c7b6f6
TrungKienekbn <culuacf009@gmail.com>
2024-11-18 07:31:31 +0700

    comm

--- src/test/repsitory.java
+++ src/test/repsitory.java
@@ -2,9 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package UserNhanVien;
+package test;
 import java.sql.*;
 import java.util.ArrayList;
+
+import UserNhanVien.Dbconn;
+import UserNhanVien.NhanVien;
 
 /**
  *
