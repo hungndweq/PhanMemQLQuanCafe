76b8330b3cc44d20a7d558b60e32c13913dddf1f
TrungKienekbn <culuacf009@gmailc.com>
2024-11-21 20:33:26 +0700

    TrungKien

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -245,6 +245,31 @@ public class PhanMemQLQuanCafe {
 
 
 
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Nhập một số nguyên: ");
+         int num = scanner.nextInt();
+         boolean isPrime = true;
+ 
+         if (num <= 1) {
+             isPrime = false;
+         } else {
+             for (int i = 2; i <= Math.sqrt(num); i++) {
+                 if (num % i == 0) {
+                     isPrime = false;
+                     break;
+                 }
+             }
+         }
+ 
+         if (isPrime) {
+             System.out.println(num + " là số nguyên tố.");
+         } else {
+             System.out.println(num + " không phải là số nguyên tố.");
+         }
+         scanner.close();
+
+         
+
     }
 
 }
