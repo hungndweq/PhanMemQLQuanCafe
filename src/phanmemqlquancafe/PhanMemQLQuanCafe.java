4881333d807c2cb409eaaaaf0f0af4eeb7d13eda
TrungKienekbn <culuacf009@gmailc.com>
2024-11-21 16:55:59 +0700

    kien em

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -211,6 +211,26 @@ public class PhanMemQLQuanCafe {
 
         
         anh em vao ma commit di nhe
+
+
+        System.out.print("Nhập tên của bạn: "); 
+        String name = scanner.nextLine(); 
+        System.out.print("Nhập tuổi của bạn: "); 
+        int age = scanner.nextInt(); 
+        scanner.nextLine(); 
+        // Đọc bỏ dòng mới còn lại 
+        System.out.print("Nhập giới tính của bạn: "); 
+        String gender = scanner.nextLine(); 
+        System.out.print("Nhập địa chỉ của bạn: "); 
+        String address = scanner.nextLine(); 
+        // In ra thông tin cá nhân 
+        System.out.println("\nThông tin cá nhân:");
+        System.out.println("Tên: " + name); 
+        System.out.println("Tuổi: " + age); 
+        System.out.println("Giới tính: " + gender);
+        System.out.println("Địa chỉ: " + address);
+
+        
     }
 
 }
