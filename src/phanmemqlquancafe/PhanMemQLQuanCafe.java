d1f0b1329c065aef3bf4b1f5c908c9c440aba6ce
TrungKienekbn <culuacf009@gmailc.com>
2024-11-21 20:15:23 +0700

    Kien em

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -240,7 +240,30 @@ public class PhanMemQLQuanCafe {
          System.out.println("Tổng của hai số là: " + sum); 
          scanner.close();
 
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Nhập một số nguyên: ");
+         int num = scanner.nextInt();
+         boolean isPrime = true;
+ 
+         if (num <= 1) {
+             isPrime = false;
+         } else {
+             for (int i = 2; i <= Math.sqrt(num); i++) {
+                 if (num % i == 0) {
+                     isPrime = false;
+                     break;
+                 }
+             }
+         }
+ 
+         if (isPrime) {
+             System.out.println(num + " là số nguyên tố.");
+         } else {
+             System.out.println(num + " không phải là số nguyên tố.");
+         }
+         scanner.close();
 
+         
     }
 
 }
