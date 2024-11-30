57e642b3ba3fcdc12293134d68d7c17b27e6d07f
TrungKienekbn <culuacf009@gmail.com>
2024-11-30 21:42:52 +0700

    thay doi commit

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -491,6 +491,19 @@ public class DoWhileExample {
             }
         }
     }
+    public class Swap {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 10;
+            
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            
+            System.out.println("a: " + a + ", b: " + b);
+        }
+    }
+    
     
         
 
