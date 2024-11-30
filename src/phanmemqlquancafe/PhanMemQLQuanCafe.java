668cea4597134bd8d79af885431cf51210500f59
TrungKienekbn <culuacf009@gmail.com>
2024-11-30 21:40:46 +0700

    Thay doi

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -483,6 +483,15 @@ public class DoWhileExample {
     }
 }
     Duy Hung di commit 53
+
+    public class Main {
+        public static void main(String[] args) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("i: " + i);
+            }
+        }
+    }
+    
         
 
 }
