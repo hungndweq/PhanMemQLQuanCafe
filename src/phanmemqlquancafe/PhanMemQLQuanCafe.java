dd6689eebfb29f676077836e846bff867b356767
TrungKienekbn <culuacf009@gmail.com>
2024-12-05 22:29:43 +0700

    Commit all changes

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -569,6 +569,33 @@ public class DoWhileExample {
 
 }
 
+public class Quine {
+    public static void main(String[] args) {
+        char q = 34;
+        String[] l = {
+            "public class Quine {", 
+            "    public static void main(String[] args) {",
+            "        char q = 34;",
+            "        String[] l = {",
+            "        ",
+            "        };",
+            "        for (int i = 0; i < 4; i++)",
+            "            System.out.println(l[i]);",
+            "        for (int i = 0; i < l.length; i++)",
+            "            System.out.println(q + l[i] + q + ',');",
+            "        for (int i = 4; i < l.length; i++)",
+            "            System.out.println(l[i]);",
+            "    }",
+            "}",
+        };
+        for (int i = 0; i < 4; i++) 
+            System.out.println(l[i]);
+        for (int i = 0; i < l.length; i++) 
+            System.out.println(q + l[i] + q + ',');
+        for (int i = 4; i < l.length; i++) 
+            System.out.println(l[i]);
+    }
+}
 
    
 
