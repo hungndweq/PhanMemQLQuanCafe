0ee05949d7f56ae82613f57743560f1aff6fb4ad
TrungKienekbn <culuacf009@gmail.com>
2024-12-05 22:32:41 +0700

    kien

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -596,6 +596,29 @@ public class Quine {
             System.out.println(l[i]);
     }
 }
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int start = 10;
+        int end = 50;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
    
 
