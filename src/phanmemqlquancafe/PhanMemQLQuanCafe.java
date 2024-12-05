9eb1f7e88233c976ba0f4af2c3573635a4a91b6f
Anh1511duc <Duclath02442@fpt.edu.vn>
2024-12-05 22:23:46 +0700

    duc commit

--- src/phanmemqlquancafe/PhanMemQLQuanCafe.java
+++ src/phanmemqlquancafe/PhanMemQLQuanCafe.java
@@ -422,6 +422,9 @@ System.out.print("Tuan Minh ngay 5");
         System.out.println("Toi la duc 4.7 "); 
         System.out.println("Toi la duc 4.8");
 
+        System.out.println("Toi la duc 4.9 "); 
+        System.out.println("Toi la duc 5.0");
+
         public class WhileLoopExample {
             public static void main(String[] args) {
                 int i = 1;
